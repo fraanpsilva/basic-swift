86139a1150b0ef940165c95218b6f06a762d271a
Francilene Silva <francilenesilva.fps10@gmail.com>
2022-05-02 19:44:41 +0100

    basic swift

---
+++ exercicios.swift
@@ -0,0 +1,12 @@
+// crie uma variável representando o nome da cidade onde você nasceu
+var cidadeNatal = "Água Branca"
+
+// crie uma constante representando o ano que você nasceu
+let anoNascimento = 1994
+
+// imprima no console a seguinte frase: "Eu nasci na cidade -Variavel cidade no ano de -constante ano-"
+// interpolação
+print("Eu nasci na cidade de \(cidadeNatal) no ano de \(anoNascimento)")
+// concatenação
+print("Eu nasci na cidade de " + cidadeNatal + ", no ano de " + String(anoNascimento))
+
---
+++ variaveis.swift
@@ -0,0 +1,25 @@
+// declarando variáveis
+var nome = "Francilene"
+var idade = 28
+
+var sobrenome = "Silva"
+idade = 29
+
+// criando constantes - o valor das constantes não pode ser alterado
+let pi = 3.14
+let cachorro = "Sparky"
+
+// imprimindo na tela
+print(idade)
+
+// concatenação
+print(nome + " " + sobrenome)
+
+// interpolação
+print ("Meu primeiro nome é \(nome), e meu último nome é \(sobrenome)")
+
+var numero1 = 10;
+var numero2 = 20
+
+print("Meu primeiro numero é: \(numero1). O meu segundo número é: \(numero2)")
+print("A soma dos dois números é  \(numero1 + numero2)")

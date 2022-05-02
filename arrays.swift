53986ff24ed6bbcc50dac1c0467015f8a52bd683
fraanpsilva <francilenesilva.pt@gmail.com>
2022-05-02 23:42:48 +0100

    arrays

---
+++ arrays.swift
@@ -0,0 +1,14 @@
+// criando arrays com tipo de dado implicita
+var anosDeCopaDoMundo = [1958, 1962, 1970, 1994, 2002]
+
+// criando array com tipo explicito
+var filmes : [String] = ["Tropa de elite", "Toy story 1, 2, 3", "Operação Big Hero"]
+
+// adicionando itens no array
+anosDeCopaDoMundo.append(2022)
+print(anosDeCopaDoMundo)
+
+filmes.append("O Artista")
+print(filmes)
+
+
